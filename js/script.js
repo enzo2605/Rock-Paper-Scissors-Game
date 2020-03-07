d16415027a34a209979d3828ff419f088e1ba321
enzo2605 <34918941+enzo2605@users.noreply.github.com>
2020-03-07 12:54:27 +0100

    Finished the major functionality of the game.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-let scoreUsr, scorePc, img_user_result, img_pc_result, user_result, pc_result, box_game_choices;
+let scoreUsrEl, scorePcEl, img_user_result, img_pc_result, user_result, pc_result, box_game_choices, scores;
+let usrScore, pcScore;
+
+scores = [0,0];
+usrScore = 0;
+pcScore = 0;
+
 
 /*** Scoreboard ***/
-scoreUsr = document.querySelector('#name-1');
-scorePc = document.querySelector('#name-2');
+scoreUsrEl = document.querySelector('#name-1');
+scorePcEl = document.querySelector('#name-2');
 
 /*** Images of scoreboard ***/
 img_user_result = document.querySelector('#user');
@@ -17,14 +23,17 @@ winner = document.querySelector('#winner');
 choices = document.querySelectorAll('.choice__img');
 box_game_choices = document.querySelector('.box-game__choices');
 
-console.log(choices);
 
 for (let i = 0; i < choices.length; i++) {
     choices[i].addEventListener('click', function () {
         let id = choices[i].getAttribute('id');
         img_user_result.src = 'img/' + id + '.png';
         //box_game_choices.style.display = 'none';
+        scores[0] = id;
+
         computer();
+
+        calculateScore(scores[0], scores[1]);
     });
 }
 
@@ -32,11 +41,42 @@ function computer() {
     var rand_choice = ['rock', 'paper', 'scissors'];
     var seed = Math.floor(Math.random() * 3);
 
-    console.log('scelta pc: ' + rand_choice[seed]);
     img_pc_result.src = 'img/' + rand_choice[seed] + '.png';
     //box_game_choices.style.display = 'block';
+    scores[1] = rand_choice[seed];
 }
 
-function calculateScore() {
-    
+function calculateScore(usr, pc) {
+
+    if((usr == 'paper' && pc == 'rock') || (usr == 'rock' && pc == 'scissors') || (usr == 'scissors' && pc == 'paper')) {
+        usrScore++;
+        user_result.textContent = usr;
+        pc_result.textContent = pc;
+        winner.textContent = ' You win!';
+        scoreUsrEl.textContent = usrScore;
+        document.querySelector('.box-game__result--text').style.color = '#06bb06';
+        console.log('user: ' + usr);
+        console.log('pc: ' + pc);
+    }
+        
+    else if((pc == 'paper' && usr == 'rock') || (pc == 'rock' && usr == 'scissors') || (pc == 'scissors' && usr == 'paper')) {
+        pcScore++;
+        user_result.textContent = usr;
+        pc_result.textContent = pc;
+        winner.textContent = ' Computer win!';
+        scorePcEl.textContent = pcScore;
+        document.querySelector('.box-game__result--text').style.color = '#EB4D4D';
+        console.log('user: ' + usr);
+        console.log('pc: ' + pc);
+    }
+        
+    else {
+        user_result.textContent = usr;
+        pc_result.textContent = pc;
+        winner.textContent = ' Parity';
+        document.querySelector('.box-game__result--text').style.color = '#fff';
+        console.log('user: ' + usr);
+        console.log('pc: ' + pc);
+    }
+        
 }
